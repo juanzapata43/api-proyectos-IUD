2a615c39ffa765472c3c11a560f9ba5b418f40b6
juanzapata43 <juan.zapata43@est.iudigital.edu.co>
2024-11-25 10:52:52 -0500

    Mejora en el esquema de universidad

--- models/university.js
+++ models/university.js
@@ -2,10 +2,13 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UniversitySchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    address: { type: String },
-    phone: { type: String },
-    creationDate: { type: Date, default: Date.now },
-    updateDate: { type: Date }
-  });
-  
+  name: { type: String, required: true, unique: true },
+  address: { type: String },
+  phone: { type: String },
+  creationDate: { type: Date, default: Date.now },
+  updateDate: { type: Date }
+});
+
+const University = mongoose.model('University', UniversitySchema);
+
+export default University;
