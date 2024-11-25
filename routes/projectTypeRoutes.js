6ae20645b5217f8ff9d69e18b77eed9a753fdec0
juanzapata43 <juan.zapata43@est.iudigital.edu.co>
2024-11-25 10:09:08 -0500

    Agregado de ruta put

--- routes/projectTypeRoutes.js
+++ routes/projectTypeRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { consultarTipoProyecto, consultarTodosTipoProyecto, crearTipoProyecto } from '../controllers/projectTypeController.js';
+import { consultarTipoProyecto, consultarTodosTipoProyecto, crearTipoProyecto, actualizarTipoProyecto } from '../controllers/projectTypeController.js';
 
 const router = express.Router();
 
 router.get('/tiposProyecto', consultarTodosTipoProyecto);
 router.get('/tiposProyecto/:id', consultarTipoProyecto);
 router.post('/tiposProyecto', crearTipoProyecto);
+router.put('/tiposProyecto/:id', actualizarTipoProyecto)
 
 export default router;
